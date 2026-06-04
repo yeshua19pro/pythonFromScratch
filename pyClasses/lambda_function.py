1c613d0ee42c982525892b46ea3b1778a4f7ff69
Yashua Narvaez <yashuanarvaez@gmail.com>
2026-06-03 21:07:27 -0500

    Adding dates, math, regex, json, try except and pip install and uninstall comments, formatting a little bit the lambda function class

--- pyClasses/lambda_function.py
+++ pyClasses/lambda_function.py
@@ -1,7 +1,5 @@
 # A lambda function is a small anonymous function.
-
 #A lambda function can take any number of arguments, but can only have one expression.
-
 # Add 10 to argument a, and return the result:
 
 x = lambda a : a + 10
